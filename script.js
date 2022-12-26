99b4c4342e6709b152913ca5eea1d5ea664d20b6
devartist <kevinwasonga415@outlook.com>
2022-12-26 19:30:10 +0300

    setting up geo loc

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-'use strict';
+"use strict"
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form")
+const containerWorkouts = document.querySelector(".workouts")
+const inputType = document.querySelector(".form__input--type")
+const inputDistance = document.querySelector(".form__input--distance")
+const inputDuration = document.querySelector(".form__input--duration")
+const inputCadence = document.querySelector(".form__input--cadence")
+const inputElevation = document.querySelector(".form__input--elevation")
+
+if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(
+  (pos) => {
+   const {latitude, longitude} = pos.coords
+
+   console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`)
+  },
+  () => {
+   alert("could get your location")
+  }
+ )
+}
