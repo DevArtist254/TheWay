8b056c510931868d19b54a66e805eef6166293b5
devartist <kevinwasonga415@outlook.com>
2022-12-27 02:10:21 +0300

    arc design 1

--- script.js
+++ script.js
@@ -11,80 +11,126 @@ const inputDuration = document.querySelector(".form__input--duration")
 const inputCadence = document.querySelector(".form__input--cadence")
 const inputElevation = document.querySelector(".form__input--elevation")
 
-let map, mapEvent
+//let map, mapEvent - changed to private properties
 
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(
-  (pos) => {
-   const {latitude, longitude} = pos.coords
+class Workouts {
+ date = new Date()
+ //Always have a uniquie id tp help in search
+ id = (Date.now + "").slice(-10)
 
-   console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`)
-
-   const coordinates = [latitude, longitude]
-
-   map = L.map("map").setView(coordinates, 13)
-
-   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-   }).addTo(map)
-
-   L.marker(coordinates)
-    .addTo(map)
-    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-    .openPopup()
-
-   map.on("click", function (mapEvt) {
-    mapEvent = mapEvt
-    form.classList.remove("hidden")
-    inputDistance.focus()
-
-    // const {lat, lng} = mapEvent.latlng
-    // const popupSettings = {
-    //  maxWidth: 250,
-    //  minWidth: 100,
-    //  autoClose: false,
-    //  closeOnClick: false,
-    //  className: "running-popup",
-    // }
-
-    // L.marker([lat, lng])
-    //  .addTo(map)
-    //  .bindPopup(L.popup(popupSettings))
-    //  .setPopupContent("run")
-    //  .openPopup()
-   })
-  },
-  () => {
-   alert("could get your location")
-  }
- )
+ constructor(coords, distance, duration) {
+  this.coords = coords // [lat, lng]
+  this.distance = distance // in km
+  this.duration = duration // in min
+ }
 }
 
-form.addEventListener("submit", (e) => {
- e.preventDefault()
-
- //clear input fields
- inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-  ""
-
- //display the marker
- const {lat, lng} = mapEvent.latlng
- const popupSettings = {
-  maxWidth: 250,
-  minWidth: 100,
-  autoClose: false,
-  closeOnClick: false,
-  className: "running-popup",
+class Running extends Workouts {
+ constructor(coords, distance, duration, cadence) {
+  super(coords, distance, duration)
+  this.cadence = cadence
+  this.calcPace()
  }
- L.marker([lat, lng])
-  .addTo(map)
-  .bindPopup(L.popup(popupSettings))
-  .setPopupContent("run")
-  .openPopup()
-})
 
-inputType.addEventListener("change", () => {
- inputElevation.closest("form__row").classList.toggle("form__row--hidden")
- inputCadence.closest("form__row").classList.toggle("form__row--hidden")
-})
+ calcPace() {
+  //min/km
+  this.pace = this.duration / this.distance
+
+  return this.pace
+ }
+}
+
+class Cycling extends Workouts {
+ constructor(coords, distance, duration, elevationGain) {
+  super(coords, distance, duration)
+  this.elevationGain = elevationGain
+  this.calcSpeed()
+ }
+
+ calcSpeed() {
+  //km/h
+  this.speed = this.distance / (this.duration / 60)
+ }
+}
+
+class App {
+ //Private properties in the class
+ #map
+ #mapEvent
+
+ constructor() {
+  this._getPosition()
+
+  form.addEventListener("submit", this._newWorkout.bind(this))
+
+  inputType.addEventListener("change", this._toggleElevationField.bind(this))
+ }
+
+ _getPosition() {
+  if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+    alert("could get your location")
+   })
+  }
+ }
+
+ _loadMap(pos) {
+  const {latitude, longitude} = pos.coords
+
+  console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`)
+
+  const coordinates = [latitude, longitude]
+
+  this.#map = L.map("map").setView(coordinates, 13)
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+   attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(this.#map)
+
+  L.marker(coordinates)
+   .addTo(this.#map)
+   .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+   .openPopup()
+
+  this.#map.on("click", this._showForm.bind(this))
+ }
+
+ _showForm(mapEvt) {
+  this.#mapEvent = mapEvt
+  form.classList.remove("hidden")
+  inputDistance.focus()
+ }
+
+ _toggleElevationField() {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden")
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden")
+ }
+
+ _newWorkout(e) {
+  e.preventDefault()
+
+  //clear input fields
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+   ""
+
+  //display the marker
+  const {lat, lng} = this.#mapEvent.latlng
+  const popupSettings = {
+   maxWidth: 250,
+   minWidth: 100,
+   autoClose: false,
+   closeOnClick: false,
+   className: "running-popup",
+  }
+  L.marker([lat, lng])
+   .addTo(this.#map)
+   .bindPopup(L.popup(popupSettings))
+   .setPopupContent("run")
+   .openPopup()
+ }
+}
+
+const app = new App()
+
+//Constursture method is called impideanlty
