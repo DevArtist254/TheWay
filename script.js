391572a2af294d8ec92b0c2b4e72287eb878636f
devartist <kevinwasonga415@outlook.com>
2022-12-27 11:05:10 +0300

    create workout 1

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ class App {
  //Private properties in the class
  #map
  #mapEvent
+ #workouts = []
 
  constructor() {
   this._getPosition()
@@ -110,23 +111,68 @@ class App {
  _newWorkout(e) {
   e.preventDefault()
 
-  //clear input fields
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-   ""
+  const validInputs = (...inputs) => inputs.every((inp) => Number.isFinite(inp))
+  const allPositive = (...inputs) => inputs.every((inp) => inp > 0)
+
+  //get data from the form
+  const type = inputType.value
+  const distance = +inputDistance.value
+  const duration = +inputDuration.value
+
+  const {lat, lng} = this.#mapEvent.latlng
+  let workout
+
+  //If runing, create a running object
+  if (type === "running") {
+   const cadence = +inputCadence.value
+
+   //check vaildity - gaurd clause
+   if (
+    !validInputs(distance, duration, cadence) ||
+    !allPositive(distance, duration, cadence)
+   )
+    return alert("Input have to be +ve numbers!")
+
+   //create a new workout
+   workout = new Running([lat, lng], distance, duration, cadence)
+  }
+
+  //if workout cycling, create cycling object
+  if (type === "cycling") {
+   const elevation = +inputElevation.value
+
+   //check vaildity
+   if (
+    // !Number.isFinite(distance) ||
+    // !Number.isFinite(duration) ||
+    // !Number.isFinite(elevation)
+    !validInputs(distance, duration, elevation) ||
+    !allPositive(distance, duration)
+   )
+    return alert("Input have to be +ve numbers!")
+
+   //create a new workout
+   workout = new Cycling([lat, lng], distance, duration, elevation)
+  }
+
+  // add new object to workout array
+  this.#workouts.push(workout)
+
+  console.log(workout)
 
   //display the marker
-  const {lat, lng} = this.#mapEvent.latlng
+
   const popupSettings = {
    maxWidth: 250,
    minWidth: 100,
    autoClose: false,
    closeOnClick: false,
-   className: "running-popup",
+   className: `${type}-popup`,
   }
   L.marker([lat, lng])
    .addTo(this.#map)
    .bindPopup(L.popup(popupSettings))
-   .setPopupContent("run")
+   .setPopupContent(`${type}`)
    .openPopup()
  }
 }
